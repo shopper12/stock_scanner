131a3178f880e7e652bdf725ee1179bb84e3e351
j <136126244+shopper12@users.noreply.github.com>
2026-05-17 00:38:11 +0900

    Apply Compose compiler plugin to app module

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -22,10 +23,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 }
 
